73f23f0b89294c94f90655fb225e414de3f5a4ed
Shekinah Shalom-Hope <shalomsheks@gmail.com>
2023-06-14 10:40:33 +0100

    Fix footer overflow

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,8 +5,9 @@ function Menu({ menu, handleBook, handleMenu }) {
     return (
         <div className={`menu ${menu && "open"}`}>
             <button onClick={handleBook}>Book</button>
-
+            <div className="book-underline"></div>
             <a href="#contact" onClick={() => handleMenu(false)}>Contact</a>
+            <div className="contact-underline"></div>
         </div>)
 }
 
